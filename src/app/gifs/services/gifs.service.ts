3af7d7a218fcaa6db28e5753829f5c2660af68c6
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-17 11:26:10 -0700

    101. Leer del LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
     private apiKey: string = 'FhztixdhZcPwJk8BzUbAmKpjJrimrpaA';
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) {
+        this.loadLocalStorage();
+     }
     
 
     get tagsHistory() {
@@ -32,6 +34,15 @@ export class GifsService {
 
     private saveLocalStorage(): void {
         localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+    }
+
+    private loadLocalStorage(): void {
+        if ( !localStorage.getItem('history') ) return;
+
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+        if ( this._tagsHistory.length === 0 ) return;
+        this.searchTag( this._tagsHistory[0] );
     }
 
     public searchTag( tag: string ): void {
