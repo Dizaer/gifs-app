8f975b67fc46206d65c6d54f1d1f25d4a13a1570
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-17 08:48:45 -0700

    95. Realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root'} )
@@ -5,8 +6,9 @@ export class GifsService {
 
     private _tagsHistory: string[] = [];
     private apiKey: string = 'FhztixdhZcPwJk8BzUbAmKpjJrimrpaA';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-    constructor() { }
+    constructor( private http: HttpClient ) { }
     
 
     get tagsHistory() {
@@ -28,6 +30,18 @@ export class GifsService {
     public searchTag( tag: string ): void {
         if ( tag.length === 0 ) return;
         this.organizeHistory( tag );
+
+        const params = new HttpParams()
+            .set('api_key', this.apiKey )
+            .set('limit', '10' )
+            .set( 'q', tag )
+
+        this.http.get(`${ this.serviceUrl  }/search`, { params })
+        .subscribe( res => {
+
+            console.log(res);
+            
+        });
     }
 
 }
