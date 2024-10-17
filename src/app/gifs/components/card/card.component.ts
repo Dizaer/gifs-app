e26dc8063da59ad6269849e1399b6202cc29b942
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-17 12:09:29 -0700

    107.  Solución a la tarea gamer plus plus compoonente
    Este va a ser el unico commit que tenga descripcion nomas porqeu sí, sera el commit mas god de tosso y con la peor hortograferia

--- src/app/gifs/components/card/card.component.ts
+++ src/app/gifs/components/card/card.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
   @Input()
   public gif!: Gif;
 
+
+  ngOnInit(): void {
+    if ( !this.gif ) throw new Error('Gif property is required');
+  }
 }
